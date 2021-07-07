07046d99d7153ac8426e5f0baa67e80f82d7255a
Rahul Kumar Ravi <rkr.rahulravi@gmail.com>
2021-07-07 17:44:49 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ io.on("connection", (socket) => {
 
     socket.join(user.room);
 
-    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    socket.emit("message", generateMessage("Server", "Welcome!"));
     socket.broadcast
       .to(user.room)
       .emit(
